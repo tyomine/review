bbcf2063b36a6197188f12c2b502ec9737220824
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-03 01:45:06 +0000

    [finish]

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,5 @@
 class Favorite < ApplicationRecord
   
   belongs_to :user
-  belongs_to :post_imege
-  
+  belongs_to :post_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    
     resources :post_comments, only: [:create, :destroy]
   end  
   
